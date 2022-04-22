9c22fb82d20b3a1317ca0c7c950f82df0686df6a
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-22 13:01:52 -0300

    Using Cart Context

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,25 @@
+//dependencies
+import { useContext } from 'react';
+//components
+import CartContext from '../../store/cart-context';
 //assets
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 
 const HeaderCardButton = (props) => {
+	//handling cart context
+	const cartCtx = useContext(CartContext);
+	const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+		return currNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
